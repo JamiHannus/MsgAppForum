84e8e8c05b35b11c11315b27330436acc7a32605
Jami Hannus <t9haja00@students.oamk.fi>
2021-04-09 18:43:45 +0300

    Started on the adapter view

---
+++ app/src/main/java/fi/oamk/msgappforum/MyAdapter.kt
@@ -0,0 +1,27 @@
+package fi.oamk.msgappforum
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.appcompat.view.menu.ActionMenuItemView
+import androidx.recyclerview.widget.RecyclerView
+
+class MyAdapter(private val myDataset: ArrayList<String>): RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
+
+    class MyViewHolder(itemView: View) :RecyclerView.ViewHolder(itemView){
+        val message: TextView = itemView.findViewById(R.id.message)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val myView = LayoutInflater.from(parent.context)
+            .inflate(R.layout.message_row,parent,false)
+        return MyViewHolder(myView)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        holder.message.text = myDataset[position]
+    }
+
+    override fun getItemCount()= myDataset.size
+}

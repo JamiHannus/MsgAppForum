1cd3eee8893fe38487fa86b2b4d7a240c4b291dc
Jami Hannus <t9haja00@students.oamk.fi>
2021-04-09 18:43:30 +0300

    The firebase works and gets the msg. also the addmessage fun works

--- app/src/main/java/fi/oamk/msgappforum/MainActivity.kt
+++ app/src/main/java/fi/oamk/msgappforum/MainActivity.kt
@@ -1,24 +1,47 @@
 package fi.oamk.msgappforum
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.style.TtsSpan
 import android.util.Log
-import com.google.firebase.database.*
+import android.view.KeyEvent
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var messages: ArrayList<String>
+    private lateinit var edMessage: EditText
+    private lateinit var database: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val database = FirebaseDatabase.getInstance()
-        val myRef = database.getReference("message")
-        myRef.setValue("hello 1!")
-        val myRef2 = database.getReference("items")
-        myRef2.get().addOnSuccessListener {
-            val items =it.value
+
+        database = Firebase.database.reference
+        edMessage = findViewById(R.id.messageText)
+        messages = arrayListOf()
+
+        edMessage.setOnKeyListener { v, keyCode, event ->
+            if ( keyCode == KeyEvent.KEYCODE_ENTER && event.action == KeyEvent.ACTION_UP){
+                Log.v("msg","msg send start")
+                addMessage()
+                return@setOnKeyListener true
+            }
+            return@setOnKeyListener false
         }
 
-
     }
+
+    private fun addMessage(){
+        val newMessage = edMessage.text.toString()
+        messages.add(newMessage)
+        database.child("messages").setValue(messages)
+            .addOnSuccessListener { Log.v("msg","msg ok") }
+            .addOnFailureListener { Log.v("msg","msg fail") }
+        edMessage.setText("")
+    }
+
 }

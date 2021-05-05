daa778db83a2cf5e59394f4fda97b7f84c8975f3
Jami Hannus <t9haja00@students.oamk.fi>
2021-05-05 23:10:36 +0300

    Rest of the stuff

--- app/src/main/java/fi/oamk/msgappforum/MainActivity.kt
+++ app/src/main/java/fi/oamk/msgappforum/MainActivity.kt
@@ -1,47 +1,181 @@
 package fi.oamk.msgappforum
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.text.InputType
 import android.util.Log
 import android.view.KeyEvent
+import android.view.Menu
+import android.view.MenuItem
+import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
+import android.widget.LinearLayout
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import com.google.firebase.database.DatabaseReference
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.*
 import com.google.firebase.database.ktx.database
+import com.google.firebase.database.ktx.getValue
 import com.google.firebase.ktx.Firebase
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.*
+import kotlin.collections.ArrayList
 
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var messages: ArrayList<String>
+    private lateinit var messages: ArrayList<Message>
     private lateinit var edMessage: EditText
+    private lateinit var rcMessageList: RecyclerView
     private lateinit var database: DatabaseReference
+    private lateinit var auth: FirebaseAuth
+    private lateinit var mAdapter: MyAdapter
+    private var currentUser: FirebaseUser? = null
+    private lateinit var key : String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        // init the late init on create
         database = Firebase.database.reference
+        auth = FirebaseAuth.getInstance()
         edMessage = findViewById(R.id.messageText)
+        rcMessageList = findViewById(R.id.recyclerView)
         messages = arrayListOf()
 
-        edMessage.setOnKeyListener { v, keyCode, event ->
-            if ( keyCode == KeyEvent.KEYCODE_ENTER && event.action == KeyEvent.ACTION_UP){
-                Log.v("msg","msg send start")
+
+        rcMessageList.layoutManager = LinearLayoutManager(this)
+        mAdapter = MyAdapter(messages)
+        rcMessageList.adapter = mAdapter
+        edMessage.setOnKeyListener { _, keyCode, event ->
+            if (keyCode == KeyEvent.KEYCODE_ENTER && event.action == KeyEvent.ACTION_UP) {
+                Log.v("msg", "msg send start")
                 addMessage()
                 return@setOnKeyListener true
             }
             return@setOnKeyListener false
         }
+        database.child("messages").addValueEventListener(
+            object : ValueEventListener {
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    if (dataSnapshot.value != null) {
+                        messages.clear()
+                        for (data in dataSnapshot.children) {
+                            Log.v("message load", data.toString())
+                            val message = data.getValue<Message>()
+                            if (message != null) {
+                                messages.add(message)
+                            }
+                        }
+                    }
+                    mAdapter.notifyDataSetChanged()
+                    rcMessageList.smoothScrollToPosition(mAdapter.itemCount-1)
+                }
 
+                override fun onCancelled(error: DatabaseError) {
+                    Log.w("reading failed", "Failed to read value.", error.toException())
+                }
+            })
     }
 
-    private fun addMessage(){
-        val newMessage = edMessage.text.toString()
+    private fun addMessage() {
+        val formatter: DateTimeFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm")
+        // val newMessage = edMessage.text.toString()
+        val newMessage = Message(
+            (edMessage.text.toString()),
+            currentUser?.email.toString(),
+            formatter.format(LocalDateTime.now())
+        )
         messages.add(newMessage)
         database.child("messages").setValue(messages)
-            .addOnSuccessListener { Log.v("msg","msg ok") }
-            .addOnFailureListener { Log.v("msg","msg fail") }
+            .addOnSuccessListener { Log.v("msg", "msg ok") }
+            .addOnFailureListener { Log.v("msg", "msg fail") }
         edMessage.setText("")
+        closeKeyBoard()
+
+
     }
 
+    private fun closeKeyBoard() {
+        val view = this.currentFocus
+        if (view != null) {
+            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(view.windowToken, 0)
+        }
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.main_activity_actions, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        R.id.action_settings -> {
+            this.showSettings()
+            true
+        }
+        else -> {
+            super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        currentUser = auth.currentUser
+        if (currentUser == null) loginDialog()
+    }
+
+    private fun loginDialog() {
+        val builder = AlertDialog.Builder(this)
+        with(builder) {
+            setTitle("Login")
+            setMessage("test@email.com & 123456")
+            val linearLayout = LinearLayout(this@MainActivity)
+            linearLayout.orientation = LinearLayout.VERTICAL
+            val inputEmail = EditText(this@MainActivity)
+            inputEmail.inputType =
+                InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS or InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS
+            inputEmail.hint = "Email"
+            linearLayout.addView(inputEmail)
+            val inputPw = EditText(this@MainActivity)
+            inputPw.inputType =
+                InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS or InputType.TYPE_TEXT_VARIATION_PASSWORD
+            inputPw.hint = "Password"
+            linearLayout.addView(inputPw)
+            builder.setView(linearLayout)
+
+            builder.setPositiveButton("Ok") { _, _ ->
+                login(inputEmail.text.toString(), inputPw.text.toString())
+            }.show()
+
+        }
+    }
+
+    private fun login(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this) { task ->
+            if (task.isSuccessful) {
+                Log.d("auth", "singInEmail Ok")
+                currentUser = auth.currentUser
+                Toast.makeText(baseContext, "Logged in :)", Toast.LENGTH_SHORT).show()
+            } else {
+                Log.w("auth", "signInwithEmail failure", task.exception)
+                Toast.makeText(baseContext, "Auth failed", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun showSettings() {
+        val intent = Intent(this, Settings::class.java).apply {
+            putExtra("currentUser", currentUser)
+        }
+        startActivity(intent)
+    }
 }
--- app/src/main/java/fi/oamk/msgappforum/MyAdapter.kt
+++ app/src/main/java/fi/oamk/msgappforum/MyAdapter.kt
@@ -1,16 +1,27 @@
 package fi.oamk.msgappforum
 
+import android.content.Context
+import android.graphics.Color
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.view.menu.ActionMenuItemView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.ktx.Firebase
 
-class MyAdapter(private val myDataset: ArrayList<String>): RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
+class MyAdapter(private val messageHistory: ArrayList<Message>):
+        RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
 
     class MyViewHolder(itemView: View) :RecyclerView.ViewHolder(itemView){
         val message: TextView = itemView.findViewById(R.id.message)
+        val author: TextView= itemView.findViewById(R.id.author)
+        //val parent: LinearLayout = itemView.findViewById(R.id.parent)
+       // val myDrawable = ContextCompat.getDrawable(itemView.context, R.drawable.rounded_corner_view)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
@@ -20,8 +31,24 @@ class MyAdapter(private val myDataset: ArrayList<String>): RecyclerView.Adapter<
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        holder.message.text = myDataset[position]
+
+        // was experimenting with making the current user one color and other msg other color
+//       val user = FirebaseAuth.getInstance().currentUser.email
+//        Log.d("email", "$user")
+//            val currentColor =
+//                if (user == messageHistory[position].author.toString()) {
+//                    Color.argb(255, 255, 0, 0)
+//                } else {
+//                    Color.argb(0, 0, 0, 0,)
+//                }
+//        holder.myDrawable?.setTint(currentColor)
+        holder.message.text = messageHistory[position].message
+        val msgAuth = messageHistory[position].author.toString()
+        val msgTime = messageHistory[position].time
+        ("By: $msgAuth on :$msgTime").also { holder.author.text = it }
+
     }
 
-    override fun getItemCount()= myDataset.size
+    override fun getItemCount()= messageHistory.size
+
 }
